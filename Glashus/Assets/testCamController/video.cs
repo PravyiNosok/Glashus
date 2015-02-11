c1ac5e1af1fb782350c9cdb7025843846969a6d6
Serge <lackshtrate@radmule.com>
2015-02-11 20:23:15 +0200

    setups

---
+++ Glashus/Assets/testCamController/video.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class video : MonoBehaviour {
+	public MovieTexture movie;
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		if (movie != null) {
+						movie.loop = true;
+						movie.Play ();
+						audio = GetComponent<AudioSource> ();
+						audio.Play ();
+				}
+	}
+}
