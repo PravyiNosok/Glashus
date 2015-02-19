3a59ff813a03cae4900c7bc945811dfaab99606d
max <3dmzvonovsky@gmail.com>
2015-02-19 12:27:03 +0200

    tutu

--- Glashus/Assets/testCamController/camflying1.cs
+++ Glashus/Assets/testCamController/camflying1.cs
@@ -10,6 +10,7 @@ public class camflying1 : MonoBehaviour {
     public Vector3 rot;
     private Vector3 tor;
 	private CharacterController chara;
+	private Vector3 posMouse;
 	// Use this for initialization
 	void Start () {
         cam = transform;
@@ -19,6 +20,7 @@ public class camflying1 : MonoBehaviour {
 		chara.height = 2;
         tor = rot = cam.eulerAngles;
         rot.z = 0;
+		posMouse = Input.mousePosition;
         cam.rotation = Quaternion.Euler(rot);
 	}
 	
@@ -26,9 +28,17 @@ public class camflying1 : MonoBehaviour {
 	void Update () {
         Vector3 dirction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
         if (dirction != Vector3.zero)
-			chara.SimpleMove(cam.TransformDirection(dirction));
-
+			chara.SimpleMove(cam.TransformDirection(dirction));						
         dirction = new Vector3(-Input.GetAxis("Mouse Y"),Input.GetAxis("Mouse X"), 0)*MouseSensitive;
+		if (dirction == Vector3.zero){
+			if (Input.touchCount != 0){
+				dirction = Input.touches[0].deltaPosition;
+			}else if (posMouse != Input.mousePosition){
+				dirction = new Vector3( -Input.mousePosition.y+posMouse.y,Input.mousePosition.x-posMouse.x, 0)*20;
+			}
+			dirction*=MouseSensitive/Screen.height;
+		}
+		posMouse = Input.mousePosition;
             rot += dirction;
             rot.x = Mathf.Clamp(rot.x, -30, 45);
             tor = Vector3.Lerp(tor, rot, 10 * Time.deltaTime);
